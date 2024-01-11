c6f2a83b7cf6624ce13cedb3cbea2739b1208fcd
georgetoloraia <gtoloraia19@gmail.com>
2024-01-11 20:47:39 +0400

    modified
    enabled covert to hex
    
    Co-Authored-By: george toloraia <17984715+giorgi1919@users.noreply.github.com>

--- autohunt.py
+++ autohunt.py
@@ -1,5 +1,3 @@
-''' Idea George Toloraia'''
-
 import random
 import time
 import subprocess
@@ -7,28 +5,27 @@ import subprocess
 def main():
     minimal = random.randint(36893488147419103232, 73786976294838206463)
     maximum = random.randint(36893488147419103232, 73786976294838206463)
-    generated = recorect(minimal, maximum)
-    gotocomand = Create_Command(generated)
+    
+    minimal_hex = hex(minimal)[2:]  # Remove '0x' prefix
+    maximum_hex = hex(maximum)[2:]  # Remove '0x' prefix
+    
+    generated = correct(minimal_hex, maximum_hex)
+    create_command(generated)
 
-def recorect(minimal, maximum) -> int:
+def correct(minimal, maximum) -> tuple:
     if minimal > maximum:
         return maximum, minimal
     else:
         return minimal, maximum
-    
-def Create_Command(text):
+
+def create_command(text):
     minimal, maximum = text
     command = f"./keynunt -m rmd160 -r {minimal}:{maximum} -f 66.rmd -R"
     
-    subprocess.run(command, shell=True, check = True)
-
-    time.sleep(4 * 60 * 60)
-
-    stop = f"^c"
-    subprocess.run(stop)
-
-    continue main()
+    try:
+        subprocess.run(command, shell=True, check=True, timeout=4 * 60 * 60)
+    except subprocess.TimeoutExpired:
+        pass
 
 if __name__ == "__main__":
     main()
-
